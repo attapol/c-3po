0eae77cfb5dae5ae50ea8730a8f1157ac10283c1
Te Rutherford <teruth@yelp.com>
2016-07-21 13:05:11 -0700

    fixed the bug

--- data_ingestor.py
+++ data_ingestor.py
@@ -1,17 +1,26 @@
 import sys
+import glob
 
 def ingest_log(file_name):
     with open(file_name) as f:
         for line in f:
-            meta_data, class_info, ts, name, text = line.split(' ||| ', 4)
-            if 'class=user' in class_info: 
-                user = class_info[11:]
-                if user != 'marley' and 'admin' not in user and 'linkified' not in text:
-                    #print line.strip()
-                    #print user
-                    print text[3:].strip()
+            try: 
+                meta_data, class_info, ts, name, text = line.split(' ||| ', 4)
+                if 'class=user' in class_info: 
+                    user = class_info[11:]
+                    if user != 'marley' and \
+                            'admin' not in user and \
+                            'jenkins' not in user and \
+                            'sensu' not in user and \
+                            'linkified' not in text:
+                        #print line.strip()
+                        #print user
+                        print text[3:].strip()
+            except ValueError:
+                pass
 
 
 if __name__ == '__main__':
-    for file_name in sys.argv[1:]:
+    files = glob.glob(sys.argv[1])
+    for file_name in files:
         ingest_log(file_name)
